cfb79bdeffc81de13534f1d9ef8c5fd64c376362
EmilBC <135125992+EmilBC@users.noreply.github.com>
2023-06-27 13:11:47 +0200

    Update DevOpsDemoApplication.java

--- src/main/java/com/devops/devOpsDemo/DevOpsDemoApplication.java
+++ src/main/java/com/devops/devOpsDemo/DevOpsDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DevOpsDemoApplication {
 
 	public static void main(String[] args) {
+		//secret added
 		//webhookok ornot
 		//-----all events
 		//comment if ok or not for push
