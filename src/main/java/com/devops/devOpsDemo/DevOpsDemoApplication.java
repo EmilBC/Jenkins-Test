5f43c77993b9b488304c62cb3b51317ef041683a
EmilBC <135125992+EmilBC@users.noreply.github.com>
2023-06-27 12:54:25 +0200

    Update DevOpsDemoApplication.java

--- src/main/java/com/devops/devOpsDemo/DevOpsDemoApplication.java
+++ src/main/java/com/devops/devOpsDemo/DevOpsDemoApplication.java
@@ -8,6 +8,7 @@ public class DevOpsDemoApplication {
 
 	public static void main(String[] args) {
 		//comment if ok or not for push
+		//One More Modif
 		SpringApplication.run(DevOpsDemoApplication.class, args);
 	}
 
